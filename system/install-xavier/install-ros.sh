67fc279d13a9b89d14c9753ed0f834147a1c1496
dheera <dheera@dheera.net>
2020-03-07 22:51:50 -0500

    update

--- system/install-xavier/install-ros.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd installROSXavier
-installROS.sh
-setupCatkinWorkspace.sh
-sudo apt-get update
-sudo apt-get install ros-melodic-web-video-server ros-melodic-rosbridge-suite ros-melodic-diagnostic-updater
--- system/install-xavier/install-tensorflow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install libhdf5-serial-dev hdf5-tools python3-pip python-pip
-sudo apt-get install zlib1g-dev zip libjpeg8-dev libhdf5-dev
-sudo pip3 install -U pip
-sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker grpcio six mock requests gast h5py astor termcolor
-
-sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
