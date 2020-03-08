b6387b2208f22f4d3bc8a7167c375899c68415c0
dheera <dheera@dheera.net>
2020-03-07 22:51:13 -0500

    update

---
+++ system/install-root.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo cp -rv root/* /
---
+++ system/install-ros.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+installROSXavier/installROS.sh
+installROSXavier/setupCatkinWorkspace.sh
+sudo apt-get update
+sudo apt-get install ros-melodic-web-video-server ros-melodic-rosbridge-suite ros-melodic-diagnostic-updater
